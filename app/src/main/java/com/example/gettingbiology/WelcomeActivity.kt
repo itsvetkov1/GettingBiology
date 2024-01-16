7188c5fb0b2af9fde1fd1110ca0c1e24dc35a25a
itsvetkov1 <i.b.cvetkov@gmail.com>
2024-01-17 00:06:30 +0200

    Stable quiz + welcome screen

---
+++ app/src/main/java/com/example/gettingbiology/WelcomeActivity.kt
@@ -0,0 +1,19 @@
+package com.example.gettingbiology
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        val startQuizButton = findViewById<Button>(R.id.startQuizButton)
+        startQuizButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
