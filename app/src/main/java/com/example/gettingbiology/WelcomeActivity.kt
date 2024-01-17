f7523324e30401b6f8dc5735804e5485d94f960b
itsvetkov1 <i.b.cvetkov@gmail.com>
2024-01-17 22:00:26 +0200

    background + coloring + welcome animation

--- app/src/main/java/com/example/gettingbiology/WelcomeActivity.kt
+++ app/src/main/java/com/example/gettingbiology/WelcomeActivity.kt
@@ -2,18 +2,44 @@ package com.example.gettingbiology
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import android.os.Handler
+import android.os.Looper
+import android.widget.RelativeLayout
+
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
+        val welcomeAnimationView = findViewById<ImageView>(R.id.welcomeAnimationView)
+        val staticImageView = findViewById<RelativeLayout>(R.id.staticImageView)
         val startQuizButton = findViewById<Button>(R.id.startQuizButton)
+
+        // Load the GIF animation
+        Glide.with(this)
+            .load(R.drawable.welcome_animation)
+            .into(welcomeAnimationView)
+
+        // Set a delay for the GIF, then show the static image and button
+        Handler(Looper.getMainLooper()).postDelayed({
+            welcomeAnimationView.visibility = View.GONE
+            staticImageView.visibility = View.VISIBLE
+        }, 3000) // 3000 milliseconds delay
+
+        // Set the click listener for the button
         startQuizButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            navigateToQuiz()
         }
+    }
+
+    private fun navigateToQuiz() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
